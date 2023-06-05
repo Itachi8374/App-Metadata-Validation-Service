940129fbc8c24b22593a2a176a6647a3b64d308c
yuva <sandeep.kothagundla@gamil.com>
2023-06-05 16:14:40 +0530

    intial code

---
+++ src/main/java/com/inmobi/appmetadata/AppMetadataApplication.java
@@ -0,0 +1,13 @@
+package com.inmobi.appmetadata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMetadataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppMetadataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inmobi/appmetadata/AppMetadataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inmobi.appmetadata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMetadataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
